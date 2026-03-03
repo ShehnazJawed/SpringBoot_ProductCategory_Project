cd8112019312ed95ed353c0123255a62976c7711
shehnazjavaid55@gmail.com <87583086+ShehnazJawed@users.noreply.github.com>
2026-03-03 09:26:45 +0500

    Logging APIs is completed

--- ProductCategories/ProductCategories/src/main/java/com/shehnaz/ProductCategories/ProductCategoriesApplication.java
+++ ProductCategories/ProductCategories/src/main/java/com/shehnaz/ProductCategories/ProductCategoriesApplication.java
@@ -4,10 +4,13 @@ package com.shehnaz.ProductCategories;
 //import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 //import io.swagger.v3.oas.annotations.info.Contact;
 //import io.swagger.v3.oas.annotations.info.Info;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.net.URL;
+import java.util.logging.Logger;
 
 //@OpenAPIDefinition(
 ////		info = @Info(
@@ -25,12 +28,21 @@ import java.net.URL;
 ////				)
 //		)
 //)
+@Slf4j
 @SpringBootApplication
 public class ProductCategoriesApplication {
 
+
+	//private static final Logger log= LoggerFactory.getLogger(ProductCategoriesApplication.class);
 	public static void main(String[] args) {
 
 		SpringApplication.run(ProductCategoriesApplication.class, args);
+//		log.info("Product Application Successfully Started!");
+//		log.warn("This is warnning message");
+//		log.debug("This is debug message");
+//		log.error("this is error message");
+//		log.trace("This is trace message");
+//
 	}
 
 }
