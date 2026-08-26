75268bc8b94733d9832f39cbf8a4a3848a6cfcab
shehnazjavaid55@gmail.com <87583086+ShehnazJawed@users.noreply.github.com>
2026-08-26 12:03:11 +0500

    Product categories Complete project java Spring boot and ReactJs

--- ProductCategories/ProductCategories/src/main/java/com/shehnaz/ProductCategories/repository/CategorieRepository.java
+++ ProductCategories/ProductCategories/src/main/java/com/shehnaz/ProductCategories/repository/CategorieRepository.java
@@ -9,4 +9,5 @@ import java.util.Optional;
 @Repository
 public interface CategorieRepository extends JpaRepository<CategorieEntity,Long> {
     Optional<CategorieEntity> findByName(String categoryName);
+    
 }
